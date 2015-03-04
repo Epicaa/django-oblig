9e96f797ac29636c8b93fa5103277b62d4d99f22
Erika <erika.sandbaek@hotmail.com>
2015-03-04 07:27:45 +0100

    Changed one line in admin.py so I can tell Django the options I want when I register the object.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
 
