0f09e517f3aed738e0eb4523801a4dc6ce26bf78
Erika <erika.sandbaek@hotmail.com>
2015-03-04 08:39:27 +0100

    Unnecessary save in sublime

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -9,11 +9,7 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date')
 
 admin.site.register(Question, QuestionAdmin)
 
