2cc159be2593d3975318431cf8e898e6b495335a
Erika <erika.sandbaek@hotmail.com>
2015-03-09 23:02:45 +0100

    Wrote code which displays the latest 5 poll questions in the system, separated by commas, according to publication date

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from polls.models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
